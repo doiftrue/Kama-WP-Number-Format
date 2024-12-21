e4bcd5c889278edc8548c2b827a215b4e4e66533
Timur Kamaev <tkama@bk.ru>
2024-12-22 04:53:17 +0500

    Update Num_Format.php
    smart() method improvements.

--- Num_Format.php
+++ Num_Format.php
@@ -17,19 +17,19 @@ interface Num_Format_Interface {
  *
  * @see number_format_i18n()
  *
- * @version 3.2
+ * @version 3.3
  */
 class Num_Format implements Num_Format_Interface {
 
 	/**
 	 * Format number. Thousands become k: 23 000 > 23k.
 	 *
+	 * @see Num_FormatTest::test__human_k()
+	 *
 	 * @param float|string $number
 	 * @param int|string   $decimals  Optional. Precision of number of decimal places. Default 0.
 	 *                                Specify string as 'decimal float_round_type': '3 flex', '2 smart', '3 fixed'
 	 *                                to set float round function. {@see _human_unit}.
-	 *
-	 * @return string
 	 */
 	public function human_k( $number, $decimals = 1 ): string {
 
@@ -42,15 +42,14 @@ class Num_Format implements Num_Format_Interface {
 	/**
 	 * Format number. Example: 23 000 000 > 23M.
 	 *
+	 * @see Num_FormatTest::test__human_short()
+	 *
 	 * @param float|string $number
 	 * @param int|string   $decimals  Optional. Precision of number of decimal places. Default 0.
 	 *                                Specify string as 'decimal float_round_type': '3 flex', '2 smart', '3 fixed'
 	 *                                to set float round function. {@see _human_unit}.
-	 *
-	 * @return string
 	 */
 	public function human_short( $number, $decimals = 1 ): string {
-
 		static $names;
 		$names || $names = [ '', 'K', 'M', 'B', 'T', 'Q' ];
 
@@ -60,37 +59,34 @@ class Num_Format implements Num_Format_Interface {
 	/**
 	 * Convert big number to readable format.
 	 *
+	 * @see Num_FormatTest::test__human_abbr()
+	 *
 	 * @param int|string $num       Original Number.
 	 * @param int|string $decimals  Optional. Precision of number of decimal places. Default 0.
 	 *                              Specify string as 'decimal float_round_type': '3 flex', '2 smart', '3 fixed'
 	 *                              to set float round function. {@see _human_unit}.
-	 *
-	 * @return string
 	 */
 	public function human_abbr( $number, $decimals = 1 ): string {
 
 		static $names;
 		$names || $names = [ '',
-			__( 'тыс.', 'hl' ),
-			__( 'млн.', 'hl' ),
-			__( 'млрд.', 'hl' ),
-			__( 'трлн.', 'hl' ),
-			__( 'квдрлн.', 'hl' ),
+			__( 'тыс.', 'hl' ),    // ths.
+			__( 'млн.', 'hl' ),    // mln.
+			__( 'млрд.', 'hl' ),   // bln.
+			__( 'трлн.', 'hl' ),   // Tn.
+			__( 'квдрлн.', 'hl' ), // Qa.
 		];
 
 		return $this->_human_unit( $number, $decimals, $names, ' %s' );
 	}
 
 	/**
-	 *
-	 * @param float|string $number    Original Number.
-	 * @param int|string   $decimals  Optional. Precision of number of decimal places. Default 0.
-	 *                                Specify string as 'decimal float_round_type': '3 flex', '2 smart', '3 fixed'
-	 *                                to set float round function.
+	 * @param float|string $number     Original Number.
+	 * @param int|string   $decimals   Optional. Precision of number of decimal places. Default 0.
+	 *                                 Specify string as 'decimal float_round_type': '3 flex', '2 smart', '3 fixed'
+	 *                                 to set float round function.
 	 * @param array        $names
-	 * @param string       $unit_patt Pattern to display units.
-	 *
-	 * @return string
+	 * @param string       $unit_patt  Pattern to display units.
 	 */
 	private function _human_unit( $number, $decimals, array $names, string $unit_patt = '' ): string {
 
@@ -115,19 +111,16 @@ class Num_Format implements Num_Format_Interface {
 			default:
 				return $this->flex( $number, $decimals ) . $unit_suffix;
 		}
-
 	}
 
 	/**
 	 * Convert big number to readable format.
 	 * Supports negative numbers.
-	 * Can be used as root function. I.e. wrap it to your
-	 * own function where pass $names and specify desired output.
+	 * Can be used as root function. I.e. wrap it to your own function where pass
+	 * $names and specify desired output.
 	 *
 	 * @param float|string $number  Original Number.
 	 * @param int          $depth   Internal.
-	 *
-	 * @return array
 	 */
 	private static function _human_unit_depth( $number, int $depth = 0 ): array {
 
@@ -145,6 +138,8 @@ class Num_Format implements Num_Format_Interface {
 	 * Format number. Smart calculate zeros after dot and
 	 * leave specified $show_decimals numbers of digits.
 	 *
+	 * @see Num_FormatTest::test__smart()
+	 *
 	 * Example:
 	 * - 0.0000000123 > 0.000000012;
 	 * - 0.0123 > 0.012;
@@ -152,8 +147,6 @@ class Num_Format implements Num_Format_Interface {
 	 *
 	 * @param float|string $number
 	 * @param int|null     $show_decimals
-	 *
-	 * @return string
 	 */
 	public function smart( $number, int $show_decimals = 2 ): string {
 
@@ -162,12 +155,15 @@ class Num_Format implements Num_Format_Interface {
 		}
 
 		$decimals = $show_decimals;
+		$abs_number = abs( $number );
+
+		if(     $abs_number < 0.1 ){  $decimals += 2; $show_decimals = 2; }
+		elseif( $abs_number < 1   ){  $decimals += 1; $show_decimals = 2; }
 
 		// use simple formats fo big numbers
-		if( $number < 30 && $number > -30 ){
-
+		if( $abs_number < 30 && $show_decimals > 1 ){
 			$number = (float) $number;
-			// increase $decimals for numbers like: n.000nnn | n.999nnn
+			// increase $decimals for numbers like: n.00nnn | n.99nnn
 			preg_match( "/\d+\.((?:0{2,}|9{2,})\d{1,$show_decimals})/", sprintf( '%.12f', $number ), $mm );
 			if( $mm ){
 				$decimals = strlen( $mm[1] );
@@ -180,11 +176,11 @@ class Num_Format implements Num_Format_Interface {
 	/**
 	 * Format number and trim ending zeros.
 	 *
+	 * @see Num_FormatTest::test__flex()
+	 * @see number_format_i18n()
+	 *
 	 * @param float|string $number
 	 * @param int          $decimals  Optional. Precision of number of decimal places. Default 0.
-	 *
-	 * @return string
-	 * @see number_format_i18n()
 	 */
 	public function flex( $number, int $decimals = 2 ): string {
 
@@ -209,11 +205,11 @@ class Num_Format implements Num_Format_Interface {
 	/**
 	 * Format number and always leave specified ending decimals.
 	 *
+	 * @see Num_FormatTest::test__fixed()
+	 * @see number_format_i18n()
+	 *
 	 * @param float|string $number
 	 * @param int          $decimals  Optional. Precision of number of decimal places. Default 0.
-	 *
-	 * @return string
-	 * @see number_format_i18n()
 	 */
 	public function fixed( $number, int $decimals = 2 ): string {
 
@@ -225,5 +221,3 @@ class Num_Format implements Num_Format_Interface {
 	}
 
 }
-
-
